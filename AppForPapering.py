d9b1125cbd333db31d7052f6093cd414440b6ef3
james3roberts <33401632+james3roberts@users.noreply.github.com>
2020-01-05 20:05:45 -0700

    Add files via upload

---
+++ AppForPapering.py
@@ -0,0 +1,31 @@
+##Script to explain step by step how to paper a house and what tools are needed unless I decide to split that up also
+import time
+print('Welcome to the Papering sections!')
+time.sleep(3)
+print('Are you ready to get started or do you need a list of tools?')
+userInput = input('ready, tools?   ')
+time.sleep(1)
+if userInput == 'tools':
+    time.sleep(2)
+    print('You will need a hammer tacker,or cap gun, knife, and paper tape. ')
+    time.sleep(2)
+    print('You will also need your ladders, pump jacks or man lift')
+    time.sleep(2)
+    print('Once you have off this you are ready to start. ')
+else:
+    print('Then lets get started with the paper.')
+
+print('Are you going to be doing this with 1 or 2 people?')
+time.sleep(1)
+userInput2 = input()
+if userInput2 =='1':
+    print('Start by unrolling the paper the entire length of the area you are papering.')
+    time.sleep(2)
+    print('Make sure that you have enough paper to extend atleat 6-8 inches past corners')
+    time.sleep(2)
+    print('so that you have plenty of room to overlap and tape the next section. ')
+    time.sleep(2)
+
+
+
+
